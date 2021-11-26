b165890bece139d5918689df4bd162420039d47c
matrixhawk <matrixhawk@users.noreply.github.com>
2021-11-26 21:45:54 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -5,6 +5,8 @@ npm run build:prod
 echo "Replace docs"
 rm -rf docs
 cp -r dist docs
+git config --global user.email "zhaoxinlei0901@gmail.com"
+git config --global user.name "matrix"
 git add docs --all
 git commit -m "Release to docs"
 git push
